9458d43eec3a7b5fbb9da26ba48c2dea52cf4a61
arundhati101 <arundhatichaudhuri1@gmail.com>
2026-02-18 00:17:16 +0530

    Updated structure

--- legal-bert-finetune/scripts/check_model.py
+++
@@ -1,8 +0,0 @@
-from transformers import AutoTokenizer, AutoModel
-
-model_name = "nlpaueb/legal-bert-base-uncased"
-
-tokenizer = AutoTokenizer.from_pretrained(model_name)
-model = AutoModel.from_pretrained(model_name)
-
-print("Legal-BERT loaded successfully!")
